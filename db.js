c9038975441cd9d154e43d1256f7235337271fcb
angelaivy <anjiwilson21@gmail.com>
2026-05-10 17:10:42 -0700

    add mongo connect uri

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 import server from './server.js';
 
-const port = process.env.PORT || 3000;
+const { PORT = 3000, MONGO_CONNECT_URI } = process.env;
 
 mongoose
-  .connect('mongodb://127.0.0.1/setlist', {})
+  .connect(MONGO_CONNECT_URI, {})
   .then(() => {
-    server.listen(port, () => {
+    server.listen(PORT, () => {
       // eslint-disable-next-line no-console
-      console.log(`Server is listening on http://localhost:${port}`);
+      console.log(`Server is listening on http://localhost:${PORT}`);
     });
   })
   .catch((e) => {
