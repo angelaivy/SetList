64f217336d1907dfcc410dc8ddf961ecfdeb94bc
angelaivy <anjiwilson21@gmail.com>
2026-05-18 19:15:36 -0700

    set connection details for local or railway

--- db.js
+++ db.js
@@ -2,10 +2,18 @@ import mongoose from 'mongoose';
 import server from './server';
 import 'dotenv/config';
 
-const { PORT = 3000, MONGO_CONNECT_URI } = process.env;
+const { PORT = 3000, MONGO_CONNECT_URI, RAILWAY_ENVIRONMENT_ID } = process.env;
+const isRailway = RAILWAY_ENVIRONMENT_ID !== undefined;
+let setConnection;
+
+if (isRailway) {
+  setConnection = MONGO_CONNECT_URI;
+} else {
+  setConnection = 'mongodb://127.0.0.1/setlist'
+}
 
 mongoose
-  .connect('mongodb://127.0.0.1/setlist', {})
+  .connect(setConnection, {})
   .then(() => {
     server.listen(PORT, () => {
       // eslint-disable-next-line no-console
