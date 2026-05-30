5acc5745b6b989d38ab7343f894482b51d93de75
angelaivy <anjiwilson21@gmail.com>
2026-05-30 15:43:24 -0700

    cleanup & update production env var

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ import artistRoutes from './routes/artist';
 import showRoutes from './routes/show';
 import eventsRoutes from './routes/events';
 import cors from 'cors'
-// import routes from './routes/auth.js';
 
 const server = express();
-// add to railway: cd deployment-sample-frontend && npm install && npm run build in build section 
-// do something similar to github actions 
 const allowedOrigins = [
   'http://localhost:5173',
 ];
 const deployedUrl = process.env.RAILWAY_PUBLIC_DOMAIN;
+/* istanbul ignore next */
 if (deployedUrl) {
   allowedOrigins.push(deployedUrl);
 }
@@ -27,6 +25,5 @@ server.use('/auth', authRoutes);
 server.use('/artist', artistRoutes);
 server.use('/show', showRoutes);
 server.use('/events', eventsRoutes);
-// server.use(routes);
 
 export default server;
